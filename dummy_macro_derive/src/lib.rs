ff98b2f309e378bfb4c7df7cbc1e9bc2fa30c0a3
jroddev <jroddev@gmail.com>
2022-09-01 17:09:12 +1000

    refactor into cleaner struture

--- dummy_macro_derive/src/lib.rs
+++ dummy_macro_derive/src/lib.rs
@@ -3,19 +3,13 @@ use quote::quote;
 use syn::{self, Field};
 extern crate proc_macro;
 
-/*
-    Group::delimiter::Brace = { }
-    Group::delimiter::Parenthesis = ( )
-    I can use these to determine dynamically which style needs to be used
-*/
-
 #[proc_macro_derive(Dummy)]
 pub fn dummy_macro_derive(input: TokenStream) -> TokenStream {
     println!("token stream: {:?}", input);
     let ast = syn::parse(input).unwrap();
     println!("----");
 
-    impl_dummy_macro(&ast)
+    impl_dummy_macro(&ast).into()
 }
 
 #[derive(Debug)]
@@ -24,7 +18,84 @@ struct FieldParams {
     data_type: syn::Ident,
 }
 
-fn build_enum(name: &syn::Ident, e: &syn::DataEnum) -> TokenStream {
+#[derive(Debug)]
+enum ConstructorType {
+    Brace(),
+    Parenthesis(),
+}
+
+#[derive(Debug)]
+struct TypeWrapper {
+    name: syn::Ident,
+    fields: Vec<FieldParams>,
+    constructor_type: ConstructorType,
+}
+
+fn construct_dummy_type(input: TypeWrapper) -> proc_macro2::TokenStream {
+    let name = input.name;
+    let fields: Vec<proc_macro2::TokenStream> =
+        input.fields.iter().map(field_to_dummy_call).collect();
+    match input.constructor_type {
+        ConstructorType::Brace() => {
+            quote! {
+                #name { #(#fields,)* }
+            }
+        }
+        ConstructorType::Parenthesis() => {
+            quote! {
+                #name ( #(#fields,)* )
+            }
+        }
+    }
+}
+
+fn field_to_dummy_call(fp: &FieldParams) -> proc_macro2::TokenStream {
+    let data_type = &fp.data_type;
+    match &fp.name {
+        Some(name) => quote! {#name: #data_type::dummy()},
+        None => quote! {#data_type::dummy()},
+    }
+}
+
+fn variant_to_type_wrapper(data: &syn::Variant) -> TypeWrapper {
+    let fields: Vec<Field> = match &data.fields {
+        syn::Fields::Named(f) => f.named.clone().into_iter().collect(),
+        syn::Fields::Unnamed(f) => f.unnamed.clone().into_iter().collect(),
+        syn::Fields::Unit => Vec::new(),
+    };
+    TypeWrapper {
+        name: data.ident.clone(),
+        fields: fields.iter().map(get_field_type).collect(),
+        constructor_type: match data.fields {
+            syn::Fields::Named(_) => ConstructorType::Brace(),
+            syn::Fields::Unnamed(_) => ConstructorType::Parenthesis(),
+            syn::Fields::Unit => ConstructorType::Brace(),
+        },
+    }
+}
+
+fn struct_to_type_wrapper(type_name: syn::Ident, data: &syn::DataStruct) -> TypeWrapper {
+    TypeWrapper {
+        name: type_name,
+        fields: data.fields.iter().map(get_field_type).collect(),
+        constructor_type: match data.fields {
+            syn::Fields::Named(_) => ConstructorType::Brace(),
+            syn::Fields::Unnamed(_) => ConstructorType::Parenthesis(),
+            syn::Fields::Unit => ConstructorType::Brace(),
+        },
+    }
+}
+
+
+fn union_to_type_wrapper(type_name: syn::Ident, data: &syn::DataUnion) -> TypeWrapper {
+    TypeWrapper {
+        name: type_name,
+        fields: data.fields.named.iter().map(get_field_type).collect(),
+        constructor_type: ConstructorType::Brace(),
+    }
+}
+
+fn build_enum(name: &syn::Ident, e: &syn::DataEnum) -> proc_macro2::TokenStream {
     let variant_count = e.variants.len() as u32;
 
     let mut index: u32 = 0;
@@ -32,28 +103,12 @@ fn build_enum(name: &syn::Ident, e: &syn::DataEnum) -> TokenStream {
         .variants
         .iter()
         .map(|v| {
-            let name = &v.ident;
-            println!("FIELDS: {:?}", &v.fields);
-            let fields = fields_blah(&v.fields);
-            let variant_line = match &v.fields {
-                syn::Fields::Named(_) => {
-                    quote! {
-                        #index => Self::#name {
-                            #(#fields,)*
-                        }
-                    }
-                }
-                syn::Fields::Unnamed(_) => {
-                    quote! {
-                        #index => Self::#name (
-                            #(#fields,)*
-                        )
-                    }
-                }
-                syn::Fields::Unit => {
-                    quote! {#index => Self::#name}
-                }
+            let wrapper = variant_to_type_wrapper(v);
+            let construct_variant = construct_dummy_type(wrapper);
+            let variant_line = quote! {
+                #index => Self::#construct_variant
             };
+
             index += 1;
             variant_line
         })
@@ -61,18 +116,13 @@ fn build_enum(name: &syn::Ident, e: &syn::DataEnum) -> TokenStream {
 
     let enum_str = name.to_string();
     quote! {
-        impl Dummy for #name {
-            fn dummy() -> Self {
-                let enum_str = #enum_str;
-                let variant_id = random::<u32>() % #variant_count;
-                match variant_id {
-                   #(#quoted,)*
-                    _ => panic!("Dummy variant {} out of bounds: {}", enum_str, variant_id)
-                }
-            }
+        let enum_str = #enum_str;
+        let variant_id = random::<u32>() % #variant_count;
+        match variant_id {
+           #(#quoted,)*
+            _ => panic!("Dummy variant {} out of bounds: {}", enum_str, variant_id)
         }
     }
-    .into()
 }
 
 fn get_field_type(field: &Field) -> FieldParams {
@@ -98,108 +148,27 @@ fn get_field_type(field: &Field) -> FieldParams {
     FieldParams { name, data_type }
 }
 
-fn fields_blah(fields: &syn::Fields) -> Vec<proc_macro2::TokenStream> {
-    let unwrapped: Vec<_> = match fields {
-        syn::Fields::Named(f) => f.named.iter().collect(),
-        syn::Fields::Unnamed(f) => f.unnamed.iter().collect(),
-        syn::Fields::Unit => Vec::new(),
-    };
-
-    let output = unwrapped
-        .iter()
-        .map(|f| {
-            let ft = get_field_type(f);
-            field_to_dummy_call(&ft)
-        })
-        .collect();
-    output
-}
-
-fn field_to_dummy_call(fp: &FieldParams) -> proc_macro2::TokenStream {
-    let data_type = &fp.data_type;
-    match &fp.name {
-        Some(name) => quote! {#name: #data_type::dummy()},
-        None => quote! {#data_type::dummy()},
-    }
-}
-
-enum BracketStyle {
-    ConstructorStyle, // { and }
-    FunctionStyle,    // ( and )
-}
-
-fn impl_dummy_macro(ast: &syn::DeriveInput) -> TokenStream {
+fn impl_dummy_macro(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     println!("ast:{:?}", ast);
-    let mut lines = Vec::new();
-    let mut bracket_style = BracketStyle::ConstructorStyle;
-    match &ast.data {
+    let name = &ast.ident;
+    let implementation = match &ast.data {
         syn::Data::Struct(s) => {
-            match &s.fields {
-                syn::Fields::Named(f) => {
-                    println!("struct::named");
-                    for param in &f.named {
-                        lines.push(get_field_type(param))
-                    }
-                }
-                syn::Fields::Unnamed(f) => {
-                    println!("struct::unnamed");
-                    for param in &f.unnamed {
-                        println!("field: {:?}", param);
-                        lines.push(get_field_type(param))
-                    }
-                    bracket_style = BracketStyle::FunctionStyle;
-                }
-                syn::Fields::Unit => {
-                    println!("struct::unit");
-                }
-            }
-        }
-        syn::Data::Union(u) => {
-            println!("union");
-            for param in &u.fields.named {
-                lines.push(get_field_type(param));
-            }
+            let wrapper = struct_to_type_wrapper(name.clone(), s);
+            construct_dummy_type(wrapper)
         }
         syn::Data::Enum(e) => {
-            println!("enum");
-            return build_enum(&ast.ident, e);
+            build_enum(&ast.ident, e)
+        }
+        syn::Data::Union(u) => {
+            let wrapper = union_to_type_wrapper(name.clone(), u);
+            construct_dummy_type(wrapper)
         }
     };
 
-    println!("lines: {:?}", lines);
-
-    let quoted: Vec<_> = lines
-        .iter()
-        .map(|line| {
-            field_to_dummy_call(line)
-        })
-        .collect();
-    println!("quoted: {:?}", quoted);
-
-    let name = &ast.ident;
-
-    let gen = match bracket_style {
-        // TODO: see note at top for Brace / Parenthesis
-        BracketStyle::ConstructorStyle => {
-            quote! {
-                impl Dummy for #name {
-                    fn dummy() -> Self {
-                        #name {
-                           #(#quoted,)*
-                        }
-                    }
-                }
-            }
-        }
-        BracketStyle::FunctionStyle => {
-            quote! {
-                impl Dummy for #name {
-                    fn dummy() -> Self {
-                        #name (
-                           #(#quoted,)*
-                        )
-                    }
-                }
+    let gen = quote! {
+        impl Dummy for #name {
+            fn dummy() -> Self {
+                #implementation
             }
         }
     };
